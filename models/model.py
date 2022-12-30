d565dfc5db59e8bc5845d22d4ae5f6e0f521ef9f
fanzhouai <44243553+fanzhouai@users.noreply.github.com>
2022-12-29 23:25:47 -0500

    Add files via upload

---
+++ models/model.py
@@ -0,0 +1,132 @@
+import numpy as np
+import torch
+import torch.nn as nn
+import torchvision
+from torchvision import models
+import torch
+from torch import nn as nn
+
+
+
+
+
+
+
+class Discriminator_network(nn.Module):
+
+    def __init__(self, in_feature):
+        super(Discriminator_network, self).__init__()
+        self.main = nn.Sequential(
+            nn.Linear(in_feature, 1024),
+            nn.ReLU(inplace=True),
+            nn.Dropout(0.5),
+            nn.Linear(1024,1024),
+            nn.ReLU(inplace=True),
+            nn.Dropout(0.5),
+            nn.Linear(1024, 1),
+            nn.Sigmoid()
+        )
+
+    def forward(self, x):
+        y = self.main(x)
+        return y
+    def get_parameters(self):
+        
+        parameter_list = [{"params":self.main.parameters(), "lr_mult":1, 'decay_mult':2}]
+
+        return parameter_list
+
+class ResNet_Discriminator_network(nn.Module):
+    """
+    AdversarialNetwork with a gredient reverse layer.
+    its ``forward`` function calls gredient reverse layer first, then applies ``self.main`` module.
+    """
+    def __init__(self, in_feature):
+        super(Discriminator_network, self).__init__()
+        self.main = nn.Sequential(
+            nn.Linear(in_feature, 1024),
+            nn.ReLU(inplace=True),
+            nn.Dropout(0.5),
+            nn.Linear(1024,1024),
+            nn.ReLU(inplace=True),
+            nn.Dropout(0.5),
+            nn.Linear(1024, 1),
+            nn.Sigmoid()
+        )
+        #self.grl = GradientReverseModule(lambda step: aToBSheduler(step, 0.0, 1.0, gamma=10, max_iter=10000))
+
+    def forward(self, x):
+        #x_ = self.grl(x)
+        y = self.main(x)
+        return y
+    
+    def get_parameters(self):
+        
+        parameter_list = [{"params":self.main.parameters(), "lr_mult":1, 'decay_mult':2}]
+
+        return parameter_list
+
+        
+
+
+
+
+
+
+class ResNet18Fc(nn.Module):
+    def __init__(self):
+        super(ResNet18Fc, self).__init__()
+        model_resnet18 = models.resnet18(pretrained=True)
+        self.conv1 = model_resnet18.conv1
+        self.bn1 = model_resnet18.bn1
+        self.relu = model_resnet18.relu
+        self.maxpool = model_resnet18.maxpool
+        self.layer1 = model_resnet18.layer1
+        self.layer2 = model_resnet18.layer2
+        self.layer3 = model_resnet18.layer3
+        self.layer4 = model_resnet18.layer4
+        self.avgpool = model_resnet18.avgpool
+        self.__in_features = model_resnet18.fc.in_features
+
+    def forward(self, x):
+        x = self.conv1(x)
+        x = self.bn1(x)
+        x = self.relu(x)
+        x = self.maxpool(x)
+        x = self.layer1(x)
+        x = self.layer2(x)
+        x = self.layer3(x)
+        x = self.layer4(x)
+        x = self.avgpool(x)
+        x = x.view(x.size(0), -1)
+        return x
+
+    def output_num(self):
+        return self.__in_features
+
+
+
+class CLS(nn.Module):
+    """
+    a two-layer MLP for classification
+    """
+    def __init__(self, in_dim, out_dim, bottle_neck_dim=256):
+        super(CLS, self).__init__()
+        self.bottleneck = nn.Linear(in_dim, bottle_neck_dim)
+        self.fc = nn.Linear(bottle_neck_dim, out_dim)
+        self.main = nn.Sequential(self.bottleneck, self.fc, nn.Softmax(dim=-1))
+
+    def forward(self, x):
+        out = [x]
+        for module in self.main.children():
+            x = module(x)
+            out.append(x)
+        return out
+    
+    def get_parameters(self):
+        
+        parameter_list = [{"params":self.main.parameters(), "lr_mult":1, 'decay_mult':2}]
+
+        return parameter_list
+    
+    
